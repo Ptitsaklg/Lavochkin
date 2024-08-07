020854682b0a713359192ca07b1e1689c5b0751a
Ptitsaklg <nfrrus13@mail.ru>
2024-08-07 21:06:03 +0300

    corrections and additions have been added

--- src/components/ChartComponent.vue
+++ src/components/ChartComponent.vue
@@ -1,112 +1,81 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12">
-        <v-btn @click="setModeHistory" :outlined="!isOnlineMode" color="primary" class="mr-6">
-          История
-        </v-btn>
-        <v-btn @click="setModeOnline" :outlined="isOnlineMode" color="primary">
-          Онлайн
-        </v-btn>
+      <v-col cols="12" class="d-flex justify-space-between align-center">
+        <div>
+          <v-btn @click="setModeHistory" :outlined="!isOnlineMode" color="primary" class="mr-6">
+            История
+          </v-btn>
+          <v-btn @click="setModeOnline" :outlined="isOnlineMode" color="primary">
+            Онлайн
+          </v-btn>
+        </div>
+        <v-switch
+          v-if="!isOnlineMode"
+          v-model="isDark"
+          color="black"
+          @click="toggleTheme"
+          label="Темная тема"
+        ></v-switch>
       </v-col>
     </v-row>
       
     <v-row v-if="!isOnlineMode">
       <v-col class="v-col-lg-6">
         <v-card>
-          <Line 
-            :options="chartOptions" 
-            :data="getChartDataCPU" 
-          />
-        </v-card>
-      </v-col>
-      <v-col class="v-col-lg-6">
-        <v-card>
-          <Line 
-            :options="chartOptions" 
-            :data="getChartDataFreeMemory" 
-          />
-        </v-card>
-      </v-col>
-      <v-col class="v-col-lg-6">
-        <v-card>
-          <Line 
-            :options="chartOptions" 
-            :data="getChartDataFreeDiskSpace" 
-          />
-        </v-card>
-      </v-col>
-      <v-col class="v-col-lg-6">
-        <v-row>
-          <v-col>
-            <v-text-field
-              v-model="startTime"
-              label="Время С"
-              type="time"
-              step="1"
-              @change="updateStartTime"
-              style="min-width: 100px;"
+          <div class="chart-container">
+            <Line 
+              ref="chartInstance"
+              :options="chartOptions" 
+              :data="getCombinedChartData" 
+              @click="handleChartClick"
             />
-          </v-col>
-          <v-col>
-            <v-text-field
-              v-model="endTime"
-              label="Время По"
-              type="time"
-              step="1"
-              @change="updateEndTime"
-              style="min-width: 100px;"
-            />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col class="text-right">
-            <v-btn
-              @click="resetTime"
-              color="primary"
-            >
-              Сбросить
-            </v-btn>
-          </v-col>
-        </v-row>
+          </div>
+          <v-row>
+            <v-col class="ma-3">
+              <v-text-field
+                v-model="startTime"
+                label="Время С"
+                type="time"
+                step="1"
+                @change="updateStartTime"
+                style="min-width: 100px;"
+              />
+            </v-col>
+            <v-col class="ma-3">
+              <v-text-field
+                v-model="endTime"
+                label="Время По"
+                type="time"
+                step="1"
+                @change="updateEndTime"
+                style="min-width: 100px;"
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="text-right mb-3 mr-3">
+              <v-btn
+                @click="resetTime"
+                color="primary"
+              >
+                Сбросить
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
-    </v-row>
 
-    <v-row v-if="isOnlineMode">
       <v-col class="v-col-lg-6">
-        <v-card>
-          <Line 
-            :options="chartOptions" 
-            :data="getChartDataCPUOnline" 
-          />
-        </v-card>
-      </v-col>
-      <v-col class="v-col-lg-6">
-        <v-card>
-          <Line 
-            :options="chartOptions" 
-            :data="getChartDataFreeMemoryOnline" 
-          />
-        </v-card>
-      </v-col>
-      <v-col class="v-col-lg-6">
-        <v-card>
-          <Line 
-            :options="chartOptions" 
-            :data="getChartDataFreeDiskSpaceOnline" 
-          />
-        </v-card>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col class="v-col-lg-12">
         <v-card>
           <v-card-title>Данные сервера</v-card-title>
           <v-data-table
             :headers="tableHeaders"
             :items="serverData"
             class="elevation-1"
+            :items-per-page-text="'Элементов на странице:'"
+            :items-per-page-all-text="'Все'"
+            :page-text="'{0}-{1} из {2}'"
           >
             <template v-slot:item.timestamp="{ item }">
               {{ formatDate(item.timestamp) }}
@@ -115,12 +84,47 @@
         </v-card>
       </v-col>
     </v-row>
+
+
+    <v-row v-if="isOnlineMode">
+      <v-col class="v-col-lg-6">
+        <v-card>
+          <div class="chart-container">
+            <Line 
+              :options="chartOptionsOnline" 
+              :data="getChartDataCPUOnline" 
+            />
+        </div>
+        </v-card>
+      </v-col>
+      <v-col class="v-col-lg-6">
+        <v-card>
+          <div class="chart-container">
+            <Line 
+              :options="chartOptionsOnline" 
+              :data="getChartDataFreeMemoryOnline" 
+            />
+        </div>
+        </v-card>
+      </v-col>
+      <v-col class="v-col-lg-6">
+        <v-card>
+          <div class="chart-container">
+            <Line 
+              :options="chartOptionsOnline" 
+              :data="getChartDataFreeDiskSpaceOnline" 
+            />
+        </div>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script setup>
 import apiClient from "@/utils/axiosConfig";
-import { ref, computed, onMounted, onUnmounted } from "vue";
+import { useTheme } from 'vuetify';
+import { ref, computed, onMounted, onUnmounted} from "vue";
 import {Line} from "vue-chartjs";
 import {
   BarElement,
@@ -138,6 +142,8 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale,
 
 const startTime = ref('00:00:00');
 const endTime = ref('23:59:59');
+const theme = useTheme();
+const isDark = ref(false);
 
 const serverData = ref([]);
 const chartLabels = ref([]);
@@ -153,12 +159,62 @@ const chartFreeDiskSpaceOnline = ref([]);
 const isOnlineMode = ref(false);
 let intervalId = null;
 
-const tableHeaders = ref([
-  { title: "Время", value: "timestamp" },
-  { title: "Загрузка процессора", value: "cpuLoad" },
-  { title: "Свободная память", value: "freeMemory" },
-  { title: "Свободное место на диске", value: "freeDiskSpace" },
-]);
+const selectedRowIndex = ref(null);
+const chartInstance = ref(null);
+
+
+function toggleTheme () {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+
+const applyTheme = () => {
+  if (!isOnlineMode.value) {
+    theme.global.name.value = isDark.value ? 'dark' : 'light';
+  } else {
+    theme.global.name.value = 'light';
+  }
+};
+
+const setModeOnline = () => {
+  isOnlineMode.value = true;
+  applyTheme();
+
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+
+  if (onlineData.value.length === 0) {
+    const baseTime = new Date();
+    baseTime.setMilliseconds(0);
+    onlineData.value = generateData(10, new Date(baseTime.getTime() - 9000)).data;
+  }
+
+  updateOnlineCharts(onlineData.value);
+
+  intervalId = setInterval(() => {
+    const currentTime = new Date();
+    currentTime.setMilliseconds(0);
+    const newData = generateData(1, currentTime).data[0];
+    onlineData.value.push(newData);
+
+    if (onlineData.value.length > 10) {
+      onlineData.value.shift();
+    }
+
+    updateOnlineCharts(onlineData.value);
+  }, 1000);
+};
+
+
+const setModeHistory = () => {
+  isOnlineMode.value = false;
+  applyTheme();
+
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+};
 
 const updateStartTime = () => {
   filterData();
@@ -199,6 +255,18 @@ const resetTime = () => {
   filterData();
 };
 
+const formatDate = (timestamp) => {
+  const date = new Date(timestamp);
+  return date.toLocaleTimeString();
+};
+
+const tableHeaders = ref([
+  { title: "Время", value: "timestamp" },
+  { title: "Загрузка процессора, %", value: "cpuLoad" },
+  { title: "Свободная память, *100 МБ", value: "freeMemory" },
+  { title: "Свободное место на диске, *100 ГБ", value: "freeDiskSpace" },
+]);
+
 
 // Имитация получения данных с сервера
 const getServerData = async () => {
@@ -227,62 +295,32 @@ const getServerData = async () => {
   }
 };
 
-const formatDate = (timestamp) => {
-  const date = new Date(timestamp);
-  return date.toLocaleString('ru-RU', {
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit'
-  });
-};
-
-const getChartDataCPU = computed(() => (
+const getCombinedChartData = computed(() => ({
+  labels: chartLabels.value,
+  datasets: [
     {
-      labels: chartLabels.value,
-      datasets: [
-        {
-          label: "Загрузка процессора",
-          borderColor: '#1b8525',
-          data: chartCPU.value,
-          pointStyle: 'circle',
-          pointRadius: 6,
-          pointHoverRadius: 10
-        },
-      ],
-    }
-));
-
-const getChartDataFreeMemory = computed(() => (
+      label: 'Загрузка процессора, %',
+      borderColor: '#42A5F5',
+      backgroundColor: '#42A5F5',
+      fill: false,
+      data: chartCPU.value,
+    },
     {
-      labels: chartLabels.value,
-      datasets: [
-        {
-          label: "Свободная память",
-          borderColor: '#175c8d',
-          data: chartFreeMemory.value,
-          pointStyle: 'circle',
-          pointRadius: 6,
-          pointHoverRadius: 10
-        },
-      ],
-    }
-));
-
-const getChartDataFreeDiskSpace = computed(() => (
+      label: 'Свободная память, *100 МБ',
+      borderColor: '#66BB6A',
+      backgroundColor: '#66BB6A',
+      fill: false,
+      data: chartFreeMemory.value,
+    },
     {
-      labels: chartLabels.value,
-      datasets: [
-        {
-          label: "Свободное место на диске",
-          borderColor: '#f5ab18',
-          data: chartFreeDiskSpace.value,
-          pointStyle: 'circle',
-          pointRadius: 6,
-          pointHoverRadius: 10
-        },
-      ],
-    }
-));
+      label: 'Свободное место на диске, *100 ГБ',
+      borderColor: '#FFCA28',
+      backgroundColor: '#FFCA28',
+      fill: false,
+      data: chartFreeDiskSpace.value,
+    },
+  ],
+}));
 
 const chartOptions = computed(() => (
     {
@@ -291,27 +329,42 @@ const chartOptions = computed(() => (
         legend: {
           position: 'top',
         },
-        title: {
-          display: true,
-          text: 'Данные с сервера',
-        },
       },
     }
 ));
 
+// Для выделения строки в таблице при клике на точку на графике 
+const handleChartClick = (event) => {
+  const chart = chartInstance.value.chart;
+  const points = chart.getElementsAtEventForMode(event, 'nearest', { intersect: true }, true);
+  if (points.length) {
+    const index = points[0].index;
+    selectedRowIndex.value = index;
+    highlightRow(index);
+  }
+};
+
+const highlightRow = (index) => {
+  const rows = document.querySelectorAll('tbody .v-data-table__tr');
+  if (index >= 0 && index < rows.length) {
+    rows.forEach((row) => {
+      row.classList.remove('highlighted-row');
+    });
+    rows[index].classList.add('highlighted-row');
+  }
+};
+
 
 // Генератор данных для режима Онлайн
-const generateData = (numPoints) => {
+const generateData = (numPoints, startTime = new Date()) => {
   const data = [];
-  const startTimestamp = new Date();
-  startTimestamp.setMilliseconds(0); // Устанавливаем миллисекунды в 0 для единообразия
+  startTime.setMilliseconds(0);
 
   for (let i = 0; i < numPoints; i++) {
-    const timestamp = new Date(startTimestamp.getTime() + i * 1000).toISOString();
-    const cpuLoad = Math.floor(Math.random() * 100); 
+    const timestamp = new Date(startTime.getTime() + i * 1000).toISOString();
+    const cpuLoad = Math.floor(Math.random() * 100);
     const freeMemory = Math.floor(Math.random() * 3000);
     const freeDiskSpace = Math.floor(Math.random() * 50000);
-
     data.push({
       timestamp,
       cpuLoad,
@@ -321,6 +374,18 @@ const generateData = (numPoints) => {
   }
 
   return { data };
+};
+
+const updateOnlineCharts = (data) => {
+  const labels = data.map(item => formatDate(item.timestamp));
+  const cpuLoadData = data.map(item => item.cpuLoad);
+  const freeMemoryData = data.map(item => item.freeMemory);
+  const freeDiskSpaceData = data.map(item => item.freeDiskSpace);
+
+  chartLabelsOnline.value = labels;
+  chartCPUOnline.value = cpuLoadData;
+  chartFreeMemoryOnline.value = freeMemoryData;
+  chartFreeDiskSpaceOnline.value = freeDiskSpaceData;
 };
 
 const getChartDataCPUOnline = computed(() => (
@@ -362,40 +427,21 @@ const getChartDataFreeDiskSpaceOnline = computed(() => (
   }
 ));
 
-const updateOnlineCharts = (data) => {
-  const labels = data.map(item => formatDate(item.timestamp));
-  const cpuLoadData = data.map(item => item.cpuLoad);
-  const freeMemoryData = data.map(item => item.freeMemory);
-  const freeDiskSpaceData = data.map(item => item.freeDiskSpace);
+const chartOptionsOnline = computed(() => (
+    {
+      responsive: true,
+      animation: {
+        duration: 10000,
+      },
+      plugins: {
+        legend: {
+          position: 'top',
+        },
 
-  chartLabelsOnline.value = labels;
-  chartCPUOnline.value = cpuLoadData;
-  chartFreeMemoryOnline.value = freeMemoryData;
-  chartFreeDiskSpaceOnline.value = freeDiskSpaceData;
-};
-
-const setModeOnline = () => {
-  isOnlineMode.value = true;
-  onlineData.value = generateData(10).data; // Генерация начальных данных
-  updateOnlineCharts(onlineData.value);
-
-  intervalId = setInterval(() => {
-    const newData = generateData(1).data[0];
-    onlineData.value.push(newData);
-    if (onlineData.value.length > 10) { // Ограничиваем до 10 последних точек данных
-      onlineData.value.shift();
+      },
     }
-    updateOnlineCharts(onlineData.value);
-  }, 1000);
-};
+));
 
-const setModeHistory = () => {
-  isOnlineMode.value = false;
-  if (intervalId) {
-    clearInterval(intervalId);
-    intervalId = null;
-  }
-};
 
 onMounted(() => {
   getServerData();
@@ -409,6 +455,13 @@ onUnmounted(() => {
 </script>
 
 
-<style scoped>
+<style lang="sass">
+.chart-container
+  position: relative
+  width: 100%
+  height: 0
+  padding-bottom: 50%
 
+.highlighted-row
+  background-color: #d3d3d3
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,7 +11,10 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-
+  theme: {
+    defaultTheme: 'light',
+  },
 })
